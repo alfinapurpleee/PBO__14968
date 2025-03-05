46350ed0780a2e1c19f989d6da05bb5ce1d02e67
alfinapurpleee <alfinadmyt1234@gmail.com>
2025-03-05 23:24:41 +0700

    first init github

---
+++ Biodata.java
@@ -0,0 +1,24 @@
+public class Biodata {
+    public static void main(String[] args) {
+        // Biodata
+        String nim = "A11.2023.14968";
+        String nama = "Alfina Damayanti";
+        String alamat = "Karangtengah,Demak";
+        String kota = "Demak";
+        String kodePos = "55161";
+        String telp = "0812-0345-6789";
+        String hp = "0876-0890-0345";
+        String email = "fina@gmail.com";
+
+        // Menampilkan Biodata
+        System.out.println("Biodata:");
+        System.out.println("NIM     : " + nim);
+        System.out.println("Nama    : " + nama);
+        System.out.println("Alamat  : " + alamat);
+        System.out.println("Kota    : " + kota);
+        System.out.println("Kode Pos: " + kodePos);
+        System.out.println("Telp    : " + telp);
+        System.out.println("HP      : " + hp);
+        System.out.println("Email   : " + email);
+    }
+}
